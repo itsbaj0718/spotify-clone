53bd0f97ea0fda539c9fdb1957fcd8011f44ab12
Bajrang <itsbaj0718@gmail.com>
2025-05-27 02:39:18 +0530

    Prepared the outline of the page

---
+++ src/app/components/song-card/song-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-song-card',
+  templateUrl: './song-card.component.html',
+  styleUrls: ['./song-card.component.css']
+})
+
+export class SongCardComponent implements OnInit {
+  @Input () public playlistThumbnail!: string;
+  @Input () public title!: string;
+  @Input () public description!: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
